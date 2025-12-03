7df316eb07c0f4414326fb8c2dab34b36a1944d8
yibolife <hanyibolife@126.com>
2025-12-04 00:01:22 +0800

    Delete agent_shoping_T/agent_shoping_T directory
    重复

--- agent_shoping_T/agent_shoping_T/app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# 导出所有模型，便于其他模块导入
-from app.models.user import User
-from app.models.agent import AgentInfo, ContactedTrip
-from app.models.shopping import ShoppingInfo, ShoppingCircle
-from app.models.binding import Binding
--- agent_shoping_T/agent_shoping_T/run.py
+++
@@ -1,7 +0,0 @@
-from app import create_app
-
-# 创建app实例（默认开发环境）
-app = create_app(config_name='development')
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
