c8448d21e97d44f606865b283ac609295784b1b6
yibolife <hanyibolife@126.com>
2026-01-03 12:48:36 +0800

    Delete agent_shoping_T directory

--- agent_shoping_T/app/forms/__init__.py
+++
@@ -1,4 +0,0 @@
-# 导出所有表单，便于路由导入
-from app.forms.auth_forms import RegistrationForm, LoginForm, AgentChoiceForm
-from app.forms.agent_forms import AgentInfoForm, AgentSubmitForm, AgentDeleteForm
-from app.forms.shopping_forms import ShoppingInfoForm, ShoppingSubmitForm, DeleteShoppingItemForm
